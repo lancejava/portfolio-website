d8412ba027a4f37f5217932b660c25782afbf3d2
lancejava <lancejava@Xubuntu.myguest.virtualbox.org>
2023-10-09 23:50:17 -0400

    Finished home page and about page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
