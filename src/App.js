c740e7a52695881fab13b64640fbeb631c11c595
lancejava <lancejava@Xubuntu.myguest.virtualbox.org>
2023-10-18 21:27:07 -0400

    Added experiences and projects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Skills from './components/Skills';
+import Work from './components/Work';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Navbar />
       <Home />
       <About />
-      <Skills/>
+      <Skills />
+      <Work />
     </div>
   );
 }
