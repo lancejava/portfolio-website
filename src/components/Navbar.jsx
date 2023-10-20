95eab32472cf2bd9499c844ce9212bd6850d70d5
lancejava <lancejava@Xubuntu.myguest.virtualbox.org>
2023-10-19 23:43:01 -0400

    Finished overall structure and Contact form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,35 @@ import React, {useState} from 'react';
 import {HiOutlineMail} from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa';
+import {Link} from 'react-scroll'
 
 export const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
 
   return (
-    <div className= 'fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300' > 
+    <div className= 'fixed w-full h-[70px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300' > 
         <div>
             <img src={Logo} alt='Logo Image' style={{width:'50px'}}/>
         </div>
         {/* menu */}
 
         <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li>
+                <Link to ="home" smooth={true} duration={500}>Home</Link>
+            </li>
+            <li>
+                <Link to ="about" smooth={true} duration={500}>About</Link>
+            </li>
+            <li>
+                <Link to ="skills" smooth={true} duration={500}>Skills</Link>
+            </li>
+            <li>
+                <Link to ="work" smooth={true} duration={500}>Work</Link>
+            </li>
+            <li>
+                <Link to ="contact" smooth={true} duration={500}>Contact</Link>
+            </li>
         </ul>
 
         {/* hamburger */}
@@ -29,11 +40,11 @@ export const Navbar = () => {
         </div>
         {/* mobile menu */}
         <ul className= {!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Projects</li>
-            <li className='py-6 text-4xl'>Contact</li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to ="home" smooth={true} duration={500}>Home</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to ="about" smooth={true} duration={500}>About</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to ="skills" smooth={true} duration={500}>Skills</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to ="work" smooth={true} duration={500}>Work</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to ="contact" smooth={true} duration={500}>Contact</Link></li>
         </ul>
 
         {/*Social icons */}
