28ae644d86dc01fba7cda045362833cef02dda29
lancejava <lancejava@Xubuntu.myguest.virtualbox.org>
2024-01-08 17:12:49 -0500

    Added Build

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
 
 export const Home = () => 
 {
     return (
         <div name='home' className='w-full h-screen px-4 bg-[#0a192f]'>
             {/* Container */}
-            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-                <p className='text-[#600000] '>Hi, my name is</p>
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center w-full h-full'>
+                <p className='text-[#8892b0] text-3xl'>Hi, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Lance Javaluyas</h1>
                 <h2 className='text-3xl sm:text-6xl font-bold text-[#8892b0]'>I'm a Full Stack  Developer.</h2>
                 <p className='text-[#8892b0] py-4 max-w[700px]'>I'm a full-stack developer specializing in building exceptional digital experiences. Currently I'm focused on building responsive full-stack web applications.</p>
                 <div>
-                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#600000] hover:border-[#600000]'>View Work
-                    <span className='group-hover:rotate-90 duration-300'>
-                        <HiArrowNarrowRight className='ml-3 ' />
-                    </span>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#600000] hover:border-[#600000]'>
+                        <Link to ="work" smooth={true} duration={500}>View Work</Link>
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <HiArrowNarrowRight className='ml-3 ' />
+                        </span>
                     </button>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
     const handleClick = () => setNav(!nav)
 
   return (
-    <div className= 'fixed w-full h-[70px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300' > 
+    <div className= 'fixed w-full h-[60px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300' style={{zIndex:1}} > 
         <div>
             <img src={Logo} alt='Logo Image' style={{width:'50px'}}/>
         </div>
@@ -52,19 +52,19 @@ export const Navbar = () => {
         <ul>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600' >
                 <a className='flex justify-between items-center w-full text-gray-300'
-                href="/">
+                href="https://www.linkedin.com/in/lancejavaluyas/">
                     Linkedin <FaLinkedin size={30} />
                 </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]' >
                 <a className='flex justify-between items-center w-full text-gray-300'
-                href="/">
+                href="https://github.com/lancejava">
                     Github <FaGithub size={30} />
                 </a>
             </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#B22222]' >
                 <a className='flex justify-between items-center w-full text-gray-300'
-                href="/">
+                href="https://mail.google.com/mail/u/0/#inbox">
                     Email <HiOutlineMail size={30} />
                 </a>
             </li>
